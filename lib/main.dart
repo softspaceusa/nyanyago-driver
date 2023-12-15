d1e3604ea18c103deb967ae19546fc6a5b236fe6
LeeKoldun <leadkoldun@gmail.com>
2023-12-15 18:10:43 +0300

    Making main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,79 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MainApp());
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+// import 'package:nanny_driver/views/home.dart';
+// import 'package:nanny_driver/views/reg.dart';
+import 'package:nanny_components/nanny_components.dart';
+import 'package:nanny_core/nanny_core.dart';
+
+void main() async { // TODO: Welcome, home and reg screen views needed
+  WidgetsFlutterBinding.ensureInitialized();
+
+  HttpOverrides.global = MyHttpOverrides();
+  
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: NannyTheme.primary
+    )
+  );
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+  Intl.defaultLocale = "ru_RU";
+  initializeDateFormatting(Intl.defaultLocale);
+  
+  DioRequest.init();
+  DioRequest.initDebugLogs();
+
+  NannyConsts.setLoginPaths([
+    LoginPath(userType: UserType.client, path: const HomeView()),
+    LoginPath(userType: UserType.admin, path: const AdminHomeView(regView: RegView())),
+  ]);
+
+  await NannyStorage.init();
+
+  Logger().d("Storage data:\nLogin data - ${(await NannyStorage.getLoginData())?.toJson()}");
+  
+  runApp(
+    MainApp(
+      firstScreen: await NannyUser.autoLogin(
+        paths: NannyConsts.availablePaths, 
+        defaultView: WelcomeView(
+          regView: const RegView(),
+          loginPaths: NannyConsts.availablePaths,
+        ),
+      ),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final Widget firstScreen;
+  
+  const MainApp({
+    super.key,
+    required this.firstScreen,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      theme: NannyTheme.appTheme,
+      home: firstScreen,
+      // home: const TestView(),
     );
+  }
+}
+
+// На случай, если Пятисотый забыл сертификаты обновить
+
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
   }
 }
